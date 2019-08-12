f9dc7a681b613d84dadae20442b225c89940bbef
quran <ran.qu@yunzhihulian.cn>
2019-08-12 20:19:08 +0800

    add log

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{HttpServer, App, web, Result};
+use actix_web::{HttpServer, App, web, Result,middleware::Logger};
 use actix_files as fs;
 
 fn index() -> Result<fs::NamedFile> {
@@ -6,17 +6,20 @@ fn index() -> Result<fs::NamedFile> {
 }
 
 fn main() -> std::io::Result<()>{
+    std::env::set_var("RUST_LOG", "actix_web=info");
+    env_logger::init();
     let sys = actix_rt::System::new("Serv");
 
     HttpServer::new(|| {
         App::new()
+            .wrap(Logger::default())
             .service(web::resource("/").route(web::get().to(index)))
             .service(fs::Files::new("/", "/static").show_files_listing())
     })
     .bind("0.0.0.0:8087")
     .unwrap()
     .start();
-
     println!("Starting HttpServer: 0.0.0.0:8087");
+
     sys.run()
 }
